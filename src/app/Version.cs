3b090b1494b1404cb1f8990584e3dae0be0d786f
bartr <bartr@microsoft.com>
2020-10-10 21:53:21 -0500

    style cop fixes

--- src/app/Version.cs
+++ src/app/Version.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 using System.Reflection;
 
 namespace CSE.WebValidate
@@ -11,6 +14,9 @@ namespace CSE.WebValidate
         // cache the assembly version
         private static string version = string.Empty;
 
+        /// <summary>
+        /// Gets assembly version
+        /// </summary>
         public static string AssemblyVersion
         {
             get
