8842d1d225c60e567e9060add704a705892a3308
bartr <bartr@microsoft.com>
2020-10-10 23:13:31 -0500

    updated crlf

--- src/app/WebValidation/Model/PerfLog.cs
+++ src/app/WebValidation/Model/PerfLog.cs
@@ -6,8 +6,13 @@ using System.Collections.Generic;
 using System.Text.Json;
 
 namespace CSE.WebValidate.Model
-{    /// <summary>    /// Performance Log class    /// </summary>    public class PerfLog
-    {        /// <summary>
+{
+    /// <summary>
+    /// Performance Log class
+    /// </summary>
+    public class PerfLog
+    {
+        /// <summary>
         /// Initializes a new instance of the <see cref="PerfLog"/> class.
         /// </summary>
         /// <param name="validationErrors">list of validation errors</param>
@@ -15,21 +20,77 @@ namespace CSE.WebValidate.Model
         {
             Errors = validationErrors;
         }
-        /// <summary>        /// Gets the Type (defaults to request)        /// </summary>
-        public static string Type => "request";        /// <summary>        /// Gets or sets the DateTime        /// </summary>
-        public DateTime Date { get; set; } = DateTime.UtcNow;        /// <summary>        /// Gets or sets the HTTP Status Code        /// </summary>
-        public int StatusCode { get; set; }        /// <summary>        /// Gets or sets a value indicating whether the failed flag        /// </summary>
-        public bool Failed { get; set; }        /// <summary>        /// Gets or sets a value indicating whether the validated flag        /// </summary>
-        public bool Validated { get; set; } = true;        /// <summary>        /// Gets the error count        /// </summary>
-        public int ErrorCount => Errors == null ? 0 : Errors.Count;        /// <summary>        /// Gets or sets the duration        /// </summary>
-        public double Duration { get; set; }        /// <summary>        /// Gets or sets the content length        /// </summary>
-        public long ContentLength { get; set; }        /// <summary>        /// Gets or sets the Category        /// </summary>
-        public string Category { get; set; }        /// <summary>        /// Gets or sets the performance quartile        /// </summary>
-        public int? Quartile { get; set; }        /// <summary>        /// Gets or sets the tag        /// </summary>
-        public string Tag { get; set; }        /// <summary>        /// Gets or sets the request path        /// </summary>
-        public string Path { get; set; }        /// <summary>        /// Gets the validation errors        /// </summary>
+
+        /// <summary>
+        /// Gets the Type (defaults to request)
+        /// </summary>
+        public static string Type => "request";
+
+        /// <summary>
+        /// Gets or sets the DateTime
+        /// </summary>
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Gets or sets the HTTP Status Code
+        /// </summary>
+        public int StatusCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the failed flag
+        /// </summary>
+        public bool Failed { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the validated flag
+        /// </summary>
+        public bool Validated { get; set; } = true;
+
+        /// <summary>
+        /// Gets the error count
+        /// </summary>
+        public int ErrorCount => Errors == null ? 0 : Errors.Count;
+
+        /// <summary>
+        /// Gets or sets the duration
+        /// </summary>
+        public double Duration { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content length
+        /// </summary>
+        public long ContentLength { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Category
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Gets or sets the performance quartile
+        /// </summary>
+        public int? Quartile { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tag
+        /// </summary>
+        public string Tag { get; set; }
+
+        /// <summary>
+        /// Gets or sets the request path
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Gets the validation errors
+        /// </summary>
         public List<string> Errors { get; }
-        /// <summary>        /// Gets the json representation of the object        /// </summary>        /// <param name="verboseErrors">include verbose errors</param>        /// <returns>json string</returns>
+
+        /// <summary>
+        /// Gets the json representation of the object
+        /// </summary>
+        /// <param name="verboseErrors">include verbose errors</param>
+        /// <returns>json string</returns>
         public string ToJson(bool verboseErrors)
         {
             JsonSerializerOptions options = new JsonSerializerOptions
