69abdac0f12a6bc68caaac471caef5707064b736
bartr <bartr@microsoft.com>
2021-04-13 19:44:06 -0500

    removed unused using

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -5,7 +5,6 @@ using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
-using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
