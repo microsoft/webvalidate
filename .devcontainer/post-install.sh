d3a4a5f7a45395aa7a86506246e341d9730f6e84
Bart Robertson <bartr@microsoft.com>
2021-07-05 15:41:21 -0500

    Updated Codespaces (#1)
    * updated codespaces
    
    * moved vscode template for clarity

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -4,11 +4,4 @@
 dotnet restore src/webvalidate.sln
 
 # copy vscode files
-mkdir -p .vscode && cp docs/vscode-template/* .vscode
-
-# install / update utilities
-DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends apt-utils dialog
-sudo apt-get install -y --no-install-recommends dnsutils httpie bash-completion curl wget git unzip
-DEBIAN_FRONTEND=dialog
+mkdir -p .vscode && cp .vscode-template/* .vscode
