d2d8e27f1527c9af79369c498187e1196efb2a72
bartr <bartr@microsoft.com>
2021-11-29 13:50:45 -0600

    updated codespaces

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -5,4 +5,8 @@ echo "on-create start" >> ~/status
 # run dotnet restore
 dotnet restore src/webvalidate.sln
 
+# pull base docker images
+docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
+docker pull mcr.microsoft.com/dotnet/sdk:6.0
+
 echo "on-create complete" >> ~/status
